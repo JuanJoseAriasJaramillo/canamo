faba3f14ff1a55da0fd9547697527a4dd2b53b45
jise <loperajosedavid14@gmail.com>
2025-05-12 21:17:13 -0500

    cambios en la calibracion y pines de entrada y salida debido a la migracion a ESP32

--- SENSORS/SENSORS.ino
+++ SENSORS/SENSORS.ino
@@ -4,21 +4,21 @@
 #define Type DHT11
 
 //pins
-const int dhtPin = 2;
-const int moisturePin = A0;
+const int dhtPin = 33;
+const int moisturePin = 32;
 
 //DHT
 DHT HT(dhtPin, Type);
 
 // moistures values
-const int dry = 595;
-const int wet = 240;
+const int dry = 4000; //aproximcion estimada de nivel de sequedad 
+const int wet = 1000; //aproximacion estimada de la humedad total del sensor 
 
 // Intervalo de tiempo
 const int dt = 1000;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     HT.begin();
 }
 
@@ -27,6 +27,7 @@ float readSoilMoisture() {
     int moistureSensorVal = analogRead(moisturePin);
     float percentageMoisture = 100.0 * (dry - moistureSensorVal) / (dry - wet);
     return constrain(percentageMoisture, 0, 100);
+    
 }
 
 //read temperature and humidity from DHT11
@@ -47,6 +48,7 @@ void displayData(float soilMoisture, float humidity, float tempC, float tempF) {
     Serial.print("°C / ");
     Serial.print(tempF);
     Serial.println("°F");
+    
 }
 
 void loop() {
@@ -55,4 +57,9 @@ void loop() {
     readDHT(humidity, tempC, tempF);
     displayData(soilMoisture, humidity, tempC, tempF);
     delay(dt);
+
+    float readmoisturePin = analogRead(moisturePin);
+    Serial.println(readmoisturePin);
+
+    
 }
